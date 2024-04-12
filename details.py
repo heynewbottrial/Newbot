d562a3b159b2a0ca6309f8a9854c42b969d18f3d
heynewbottrial <164299318+heynewbottrial@users.noreply.github.com>
2024-04-13 00:28:33 +0530

    Add files via upload

---
+++ details.py
@@ -0,0 +1,9 @@
+import os
+
+api_id = 4942197
+api_hash = "13248a2c551b73193969b42194023635"
+bot_token = "6913163552:AAHy8Tmi-HYELZkViq0rUVLQ7cTWjYJoiMk"
+auth_users = [5892781710]
+sudo_user = 5892781710
+log_channel = -1002073188657
+txt_channel = -1002001856626
